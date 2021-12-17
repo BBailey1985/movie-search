0455e7022ccfdd4dd52bdcc4826b8af724ade6f3
BooMajka <maya@Marias-iMac.local>
2021-12-16 20:53:09 -0600

    displaying 10 movies

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,75 +4,82 @@ var searchButtonEl = document.querySelector("#search-button");
 var watchListEl = document.querySelector("#watch-list");
 var repoListEl = document.querySelector("#repo-list");
 var warningEl = document.querySelector(".input-field");
-var warningParagraphEl=document.querySelector(".warning");
+var warningParagraphEl = document.querySelector(".warning");
 //API Keys to pull in data
-var titleApiKey = "c0542bf1";
-var genreApiKey = "1d758f3d2b1a8c8efada332dc1acd449";
+var ApiKey = "1d758f3d2b1a8c8efada332dc1acd449";
 
-//submit form handler 
-var submitFormHandler= function(event){
-event.preventDefault();
-var movie=searchBoxEl.value.toLowerCase().trim()
+//submit form handler
+var submitFormHandler = function (event) {
+  event.preventDefault();
+  var movie = searchBoxEl.value.toLowerCase().trim();
 
-if (movie){
+  if (movie) {
     warningParagraphEl.classList.remove("show");
-    getMovie()
-   
-}else{ warningParagraphEl.classList.add("show");
-}
-}
-  
+    repoListEl.innerHTML = "";
+    getMovie(movie);
+  } else {
+    warningParagraphEl.classList.add("show");
+  }
+};
 
 // function to search movie title
-var getMovie = function(titleName) {
-  var apiUrl = "https://www.omdbapi.com/?apikey=" + titleApiKey + "&s=" + titleName;
+var getMovie = function (titleName) {
+  var apiUrl =
+    "https://api.themoviedb.org/3/search/movie?&api_key=" +
+    ApiKey +
+    "&query=" +
+    titleName;
 
   //fetch API for titles and then display data
-  fetch(apiUrl).then(function(response) {
-    console.log(response);
-      if(response.ok){
-        response.json().then(function(data) {
-            displayTitles(data.Search)
-            // console.log(data);
-  })
-      }else {
-          console.log(response.statusText)
-      }
-  // fetch API for genres
-  var genreApiUrl = "https://api.themoviedb.org/3/search/movie?api_key=" + genreApiKey + "&query=" + titleName;
-  fetch(genreApiUrl).then(function(response) {
-    response.json().then(function (data) {
-      console.log(data);
-   });
-  });
-});
-}
-
-//display movie titles
-var displayTitles = function(titles) {
-    for(var i = 0; i < titles.length; i++) {
-      var moviesListItem = document.createElement("div");
-      var moviesThumb = document.createElement("img");
-      var moviesInfo = document.createElement("div");
-      if (titles[i].Poster == "N/A") {
-        moviesThumb.src = "assets/img/image-not-found.png"
-      } else {
-        moviesThumb.src=titles[i].Poster;
-      }         
-      moviesInfo.innerHTML = '<h4>' + titles[i].Title + '</h4><h5>' + titles[i].Year + '</h5>'
-      moviesListItem.append(moviesThumb);
-      moviesListItem.append(moviesInfo); 
-      moviesListItem.setAttribute("class", "card")
-      repoListEl.appendChild(moviesListItem);
-
+  fetch(apiUrl).then(function (response) {
+    if (response.ok) {
+      response.json().then(function (data) {
+        displayTitles(data.results);
+        // console.log(data);
+      });
+    } else {
+      console.log(response.statusText);
     }
-  
-}
+    // fetch API for genres
+    // var genreApiUrl = "https://api.themoviedb.org/3/search/movie?api_key=" + ApiKey + "&query=" + titleName;
+    // fetch(genreApiUrl).then(function(response) {
+    //   response.json().then(function (data) {
+    //     console.log(data);
+    //  });
+    // });
+  });
+};
 
+// display movie titles
+var displayTitles = function (titles) {
+  var display = titles.splice(10);
+  display.forEach((el) => {
+    var moviesListItem = document.createElement("div");
+    var moviesThumb = document.createElement("img");
+    moviesListItem.style.display = "flex";
+    moviesListItem.style.padding = "10px";
+    moviesThumb.style.marginRight = "10px";
+    var moviesInfo = document.createElement("div");
+    if (el.poster_path == null) {
+      moviesThumb.setAttribute("src", "./image-not-found.png");
+      moviesThumb.style.width = "50px";
+      moviesThumb.style.height = "70px";
+    } else {
+      moviesThumb.setAttribute(
+        "src",
+        "https://image.tmdb.org/t/p/original" + el.poster_path
+      );
+      moviesThumb.style.width = "50px";
+      moviesThumb.style.height = "70px";
+    }
+    moviesInfo.innerHTML =
+      "<p>" + el.title + "</p><p>" + el.release_date + "</p>";
+    moviesListItem.append(moviesThumb);
+    moviesListItem.append(moviesInfo);
+    moviesListItem.setAttribute("class", "card");
+    repoListEl.appendChild(moviesListItem);
+  });
+};
 
 // submit button
-searchButtonEl.addEventListener("click", function () {
-  var selectedTitle = searchBoxEl.value;
-  getMovie(selectedTitle);
-  searchBoxEl.value ="";
-});
+searchButtonEl.addEventListener("click", submitFormHandler);
