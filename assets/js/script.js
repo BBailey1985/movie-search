b4a39fb61ba0d3702ce7bb927a27a683678ba335
samini1 <92944248+samini1@users.noreply.github.com>
2021-12-19 17:54:09 -0600

    Got the modal to display the data which we wanted it to Started trying to add an event listener to the modal button to display things in the watchlist

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,75 +4,119 @@ var searchButtonEl = document.querySelector("#search-button");
 var watchListEl = document.querySelector("#watch-list");
 var repoListEl = document.querySelector("#repo-list");
 var warningEl = document.querySelector(".input-field");
-var warningParagraphEl=document.querySelector(".warning");
+var warningParagraphEl = document.querySelector(".warning");
+var titleInfoEl = document.querySelector("#title-info");
+var titleSummary = document.querySelector("#title-summary");
+var modalTitleEl = document.querySelector("#modal-title");
+var modalSummaryEl = document.querySelector("#modal-summary");
+var modalImageEl= document.querySelector("#modal-image");
+var modalButtonEl = document.querySelector("#modal-button");
+var savedMovies = [];
+
 //API Keys to pull in data
-var titleApiKey = "c0542bf1";
-var genreApiKey = "1d758f3d2b1a8c8efada332dc1acd449";
+var ApiKey = "1d758f3d2b1a8c8efada332dc1acd449";
 
-//submit form handler 
-var submitFormHandler= function(event){
-event.preventDefault();
-var movie=searchBoxEl.value.toLowerCase().trim()
+//submit form handler
+var submitFormHandler = function (event) {
+  event.preventDefault();
+  var movie = searchBoxEl.value.toLowerCase().trim();
 
-if (movie){
+  if (movie) {
     warningParagraphEl.classList.remove("show");
-    getMovie()
-   
-}else{ warningParagraphEl.classList.add("show");
-}
-}
-  
+    repoListEl.innerHTML = "";
+    getMovie(movie);
+  } else {
+    warningParagraphEl.classList.add("show");
+  }
+};
 
 // function to search movie title
-var getMovie = function(titleName) {
-  var apiUrl = "https://www.omdbapi.com/?apikey=" + titleApiKey + "&s=" + titleName;
+var getMovie = function (titleName) {
+  var apiUrl =
+    "https://api.themoviedb.org/3/search/movie?&api_key=" +
+    ApiKey +
+    "&query=" +
+    titleName;
 
   //fetch API for titles and then display data
-  fetch(apiUrl).then(function(response) {
-    console.log(response);
-      if(response.ok){
-        response.json().then(function(data) {
-            displayTitles(data.Search)
-            console.log(data);
-  })
-      }else {
-          console.log(response.statusText)
-      }
-  // fetch API for genres
-  var genreApiUrl = "https://api.themoviedb.org/3/search/movie?api_key=" + genreApiKey + "&query=" + titleName;
-  fetch(genreApiUrl).then(function(response) {
-    response.json().then(function (data) {
-      console.log(data);
-   });
-  });
-});
-}
-
-//display movie titles
-var displayTitles = function(titles) {
-    for(var i = 0; i < titles.length; i++) {
-      var moviesListItem = document.createElement("div");
-      var moviesThumb = document.createElement("img");
-      var moviesInfo = document.createElement("div");
-      if (titles[i].Poster == "N/A") {
-        moviesThumb.src = "assets/img/image-not-found.png"
-      } else {
-        moviesThumb.src=titles[i].Poster;
-      }         
-      moviesInfo.innerHTML = '<div><h5>' + titles[i].Title + '<h5><p class = "card-title">' + titles[i].Year + '</p>'
-      moviesListItem.append(moviesInfo); 
-      moviesListItem.append(moviesThumb);      
-      moviesListItem.setAttribute("class", "card");          
-      repoListEl.appendChild(moviesListItem);
-
+  fetch(apiUrl).then(function (response) {
+    if (response.ok) {
+      response.json().then(function (data) {
+        displayTitles(data.results);
+        console.log(data);
+      });
+    } else {
+      console.log(response.statusText);
     }
-  
-}
+    // fetch API for genres
+    // var genreApiUrl = "https://api.themoviedb.org/3/search/movie?api_key=" + ApiKey + "&query=" + titleName;
+    // fetch(genreApiUrl).then(function(response) {
+    //   response.json().then(function (data) {
+    //     console.log(data);
+    //  });
+    // });
+  });
+};
 
+// display movie titles
+var displayTitles = function (titles) {
+  var display = titles.splice(10);
+  display.forEach((el) => {
+    var moviesListItem = document.createElement("div");
+    var moviesThumb = document.createElement("img");
+    moviesListItem.style.display = "flex";
+    moviesListItem.style.padding = "10px";
+    moviesThumb.style.marginRight = "10px";
+    var moviesInfo = document.createElement("div");
+    if (el.poster_path == null) {
+      moviesThumb.setAttribute("src", "./image-not-found.png");
+      moviesThumb.style.width = "50px";
+      moviesThumb.style.height = "70px";
+    } else {
+      moviesThumb.setAttribute(
+        "src",
+        "https://image.tmdb.org/t/p/original" + el.poster_path
+      );
+      moviesThumb.style.width = "50px";
+      moviesThumb.style.height = "70px";
+    }
+    moviesInfo.innerHTML =
+      "<p>" + el.title + "</p><p>" + el.release_date + "</p>";
+    moviesListItem.append(moviesThumb);
+    moviesListItem.append(moviesInfo);
+    moviesListItem.setAttribute("class", "card");
+    repoListEl.appendChild(moviesListItem);
+
+    moviesListItem.addEventListener("click", function(event) {
+      console.log(event.target)
+      var elem = document.querySelector("#modal1");
+      var instance = M.Modal.init(elem);
+      instance.open()
+      modalTitleEl.textContent = el.title;
+      modalSummaryEl.textContent = el.overview;
+      modalImageEl.setAttribute( "src", "https://image.tmdb.org/t/p/original" + el.poster_path)
+      modalButtonEl.addEventListener("click", function (){
+        saveLocalStorage(el)
+        // we were having trouble callign the data instantly when modal initiated. 
+        //By binding the function to the event lister we are supposedly calling it there. 
+        //Supposedly by writing it the above way instead we are now assigning the function to a handler.
+        //I don't fully understand, but maybe it does what we want now?
+      });     
+         
+    });    
+
+  });
+};
+
+
+function saveLocalStorage (item) { 
+  console.log(item);     
+    var savedFilms = JSON.parse(localStorage.getItem("savedMovies")) || [];
+    if (!savedFilms.includes(item)) {
+      savedFilms.push(item) 
+    } 
+    localStorage.setItem("savedMovies", JSON.stringify(savedFilms))
+}
 
 // submit button
-searchButtonEl.addEventListener("click", function () {
-  var selectedTitle = searchBoxEl.value;
-  getMovie(selectedTitle);
-  searchBoxEl.value ="";
-});
+searchButtonEl.addEventListener("click", submitFormHandler);
